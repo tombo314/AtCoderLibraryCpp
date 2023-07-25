c81143bf6ea6eee86f5e2e61618b92d16fcbfed5
tombo314 <u_tombo@icloud.com>
2023-07-26 08:13:48 +0900

    Add base_repr

---
+++ BaseRepr.cpp
@@ -0,0 +1,16 @@
+string base_repr(ll n, ll base){
+    /*
+    10進数のnをbase進数に変換する
+    O(logn)
+    */
+    ll exp = 0;
+    while (pow_(base, exp)<n) exp++;
+    string ret;
+    while (exp>=0){
+        ll m = n/pow_(base, exp);
+        ret += to_string(m);
+        n -= m*pow_(base, exp);
+        exp--;
+    }
+    return ret;
+}
