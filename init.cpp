e007ce98a770d33e21e7d2bda61ca53608038238
tombo314 <u_tombo@icloud.com>
2023-07-26 16:56:17 +0900

    Add srt(), rsrt(), copy()

--- init.cpp
+++ init.cpp
@@ -3,6 +3,9 @@ typedef long long ll;
 #define rep(i, n) for (ll i=0; i<n; i++)
 #define rrep(i, n) for (ll i=n-1; i>=0; i--)
 #define vrep(x, v) for (auto x: v)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.begin(), v.end(), greater<ll>())
+#define copy(v1, v2) copy(v1.begin(), v1.end(), v2.begin())
 #define inf ll(1e18)
 using namespace std;
 
