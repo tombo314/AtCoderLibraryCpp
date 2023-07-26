4b2ea18b2808cfabcd0acf16721a847de4ee92ec
tombo314 <u_tombo@icloud.com>
2023-07-26 12:15:28 +0900

    Add BellmanFord.cpp

---
+++ BellmanFord.cpp
@@ -0,0 +1,26 @@
+vector<ll> bellman_ford(vector<vector<ll>> g, ll n, ll start=0){
+    /*
+    ベルマンフォード法 O(VE) (0-indexed)
+    g: グラフ [[始点, 終点, コスト], ...]
+    n: 頂点数
+    start: 開始する頂点
+    */
+    vector<ll> dist(n, inf);
+    dist[start] = 0;
+    rep(i, n){
+        bool update = false;
+        vrep(v, g){
+            ll x,y,c;
+            x = v[0];
+            y = v[1];
+            c = v[2];
+            if (dist[y]>dist[x]+c){
+                dist[y] = dist[x]+c;
+                update = true;
+            }
+        }
+        if (!update) break;
+        if (i==n-1) return {-1};
+    }
+    return dist;
+}
