41c536eb0c7bc3dc606fbd68557d735580a95529
tombo314 <u_tombo@icloud.com>
2023-07-30 20:50:56 +0900

    Add Pow_.cpp

---
+++ Pow_.cpp
@@ -0,0 +1,11 @@
+ll pow_(ll x, ll n, ll mod=1e18){
+    ll ret = 1;
+    while (n>0){
+        if (n&1){
+            ret = ret*x%mod;
+        }
+        x = x*x%mod;
+        n >>= 1;
+    }
+    return ret;
+}
