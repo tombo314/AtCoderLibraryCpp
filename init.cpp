24953e43334a81197028649d4a2245e655588a10
tombo314 <u_tombo@icloud.com>
2023-07-30 20:51:26 +0900

    Remove pow_()

--- init.cpp
+++ init.cpp
@@ -12,18 +12,6 @@ typedef long long ll;
 #define inf ll(1e18)
 using namespace std;
 
-ll pow_(ll x, ll n, ll mod=1e18){
-    ll ret = 1;
-    while (n>0){
-        if (n&1){
-            ret = ret*x%mod;
-        }
-        x = x*x%mod;
-        n >>= 1;
-    }
-    return ret;
-}
-
 int main(){
     
 }
