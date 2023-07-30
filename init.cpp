8e10b5d3c535f3ad0ff83ef9af37042563486853
tombo314 <u_tombo@icloud.com>
2023-07-30 23:58:00 +0900

    Add bpq, spq

--- init.cpp
+++ init.cpp
@@ -8,6 +8,8 @@ typedef long long ll;
 #define copy(v1, v2) copy(v1.begin(), v1.end(), v2.begin())
 #define vmax(v) *max_element(v.begin(), v.end())
 #define vmin(v) *min_element(v.begin(), v.end())
+#define bpq(v, T) priority_queue<T> v
+#define spq(v, T) priority_queue<T, vector<T>, greater<T>> v
 #define pb push_back
 #define inf ll(1e18)
 using namespace std;
