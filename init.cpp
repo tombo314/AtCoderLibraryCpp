9df86208e397491c972975380a940d9ad42e670a
tombo314 <u_tombo@icloud.com>
2023-07-30 16:21:34 +0900

    Add max_el(), min_el()

--- init.cpp
+++ init.cpp
@@ -6,6 +6,8 @@ typedef long long ll;
 #define srt(v) sort(v.begin(), v.end())
 #define rsrt(v) sort(v.begin(), v.end(), greater<ll>())
 #define copy(v1, v2) copy(v1.begin(), v1.end(), v2.begin())
+#define max_el(v) *max_element(v.begin(), v.end())
+#define min_el(v) *min_element(v.begin(), v.end())
 #define pb push_back
 #define inf ll(1e18)
 using namespace std;
