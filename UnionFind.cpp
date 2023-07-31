e788d71eec5f2aa78e69ffddcd9d7a78935e2c11
tombo314 <u_tombo@icloud.com>
2023-08-01 07:52:19 +0900

    Add UnionFind.cpp

---
+++ UnionFind.cpp
@@ -0,0 +1,39 @@
+class UnionFind {
+public:
+    ll n;
+    vector<ll> par;
+    vector<ll> siz;
+
+    UnionFind(ll k){
+        n = k;
+        par.resize(n+1, -1);
+        siz.resize(n+1, 1);
+    }
+
+    ll find(ll x){
+        while (par[x]!=-1) x = par[x];
+        return x;
+    }
+
+    void unite(ll u, ll v){
+        ll ru = find(u);
+        ll rv = find(v);
+        if (ru==rv) { return; }
+        
+        if (siz[ru]<siz[rv]){
+            par[ru] = rv;
+            siz[rv] += siz[ru];
+        }
+        else {
+            par[rv] = ru;
+            siz[ru] += siz[rv];
+        }
+    }
+
+    bool same(ll u, ll v){
+        if (find(u)==find(v)) {return 1;}
+        return 0;
+    }
+
+    ll get_size(ll u){ return siz[find(u)]; }
+};
