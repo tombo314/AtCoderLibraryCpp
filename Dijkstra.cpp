d810f0f3ce9ff521dc678c38f96ec770c2c48f5d
tombo314 <u_tombo@icloud.com>
2023-08-01 10:06:36 +0900

    Update

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -7,18 +7,17 @@ vector<ll> dijkstra(vector<vector<vector<ll>>> g, ll n, ll start=1){
     */
     vector<ll> dist(n+1, inf);
     dist[start] = 0;
-    vector<bool> used(n+1, false);
-    used[start] = true;
-    priority_queue<vector<ll>, vector<vector<ll>>, greater<vector<ll>>> q;
+    vector<bool> used(n+1, 0);
+    used[start] = 1;
+    spq(q, vector<ll>);
     vrep(x, g[start]) q.push(x);
     while (!q.empty()){
         ll c = q.top()[0];
         ll v = q.top()[1];
-        cout << c << " " << v << endl;
         q.pop();
         if (used[v]) continue;
         dist[v] = c;
-        used[v] = true;
+        used[v] = 1;
         vrep(x, g[v]){
             if (!used[x[1]]) q.push({x[0]+dist[v], x[1]});
         }
