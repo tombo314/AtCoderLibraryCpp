d40e6561d4144704c40e003905b070332a5b45c4
tombo314 <u_tombo@icloud.com>
2023-08-01 10:45:28 +0900

    Add pow_

--- BaseRepr.cpp
+++ BaseRepr.cpp
@@ -1,3 +1,15 @@
+ll pow_(ll x, ll n, ll mod=1e18){
+    ll ret = 1;
+    while (n>0){
+        if (n&1){
+            ret = ret*x%mod;
+        }
+        x = x*x%mod;
+        n >>= 1;
+    }
+    return ret;
+}
+
 string base_repr(ll n, ll base){
     /*
     10進数のnをbase進数に変換する
