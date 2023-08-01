b2aad788b043e8f687fa02907fe9b3e203f1f59d
tombo314 <u_tombo@icloud.com>
2023-08-01 11:25:41 +0900

    Add ModInt.cpp

---
+++ ModInt.cpp
@@ -0,0 +1,59 @@
+class mint {
+    ll x;
+    ll mod;
+public:
+    mint(ll n, ll m=998244353) : x((x%mod+mod)%mod) {
+        x = n;
+        mod = m;
+    }
+    mint operator-() const { 
+      return mint(-x);
+    }
+    mint& operator+=(const mint& a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint& a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const  mint& a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint& a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint& a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint& a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const {
+        return pow(mod-2);
+    }
+    mint& operator/=(const mint& a) {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint& a) const {
+        mint res(*this);
+        return res/=a;
+    }
+
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.x;
+        return os;
+    }
+};
