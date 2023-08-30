f1e5fbac98a3dba2d988929925bf8eebcd1225aa
tombo314 <u_tombo@icloud.com>
2023-08-30 23:26:33 +0900

    Update rsrt()

--- init.cpp
+++ init.cpp
@@ -7,7 +7,7 @@ typedef long long ll;
 #define rrep(i, a, b) for (ll i=a; i>b; i--)
 #define vrep(x, v) for (auto x: v)
 #define srt(v) sort(v.begin(), v.end())
-#define rsrt(v) sort(v.begin(), v.end(), greater<ll>())
+#define rsrt(v, T) sort(v.begin(), v.end(), greater<T>())
 #define copy(v1, v2) copy(v1.begin(), v1.end(), v2.begin())
 #define vmax(v) *max_element(v.begin(), v.end())
 #define vmin(v) *min_element(v.begin(), v.end())
