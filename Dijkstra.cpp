c10b5e02635f7d214eefe3cb64fbd1cd621d9fa3
tombo314 <u_tombo@icloud.com>
2023-11-19 12:58:39 +0900

    Update 経路復元、クラス化

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -2,25 +2,52 @@
 (1-indexed) O((V+E)logV)
 g: グラフ [[[cost, node], [cost, node]], ...]
 n: ノード数
-start: スタートするノード
+start: スタートするノード (start=1)
 */
-vector<ll> dijkstra(vector<vector<vector<ll>>> g, ll n, ll start=1){
-    vector<ll> dist(n+1, inf);
-    dist[start] = 0;
-    vector<bool> used(n+1, 0);
-    used[start] = 1;
-    spq(q, vector<ll>);
-    vrep(x, g[start]) q.push(x);
-    while (!q.empty()){
-        ll c = q.top()[0];
-        ll v = q.top()[1];
-        q.pop();
-        if (used[v]) continue;
-        dist[v] = c;
-        used[v] = 1;
-        vrep(x, g[v]){
-            if (!used[x[1]]) q.push({x[0]+dist[v], x[1]});
+struct Dijkstra {
+
+    ll n;
+    vector<vector<pair<ll, ll>>> g;
+    vector<ll> dist, trace;
+    priority_queue<tuple<ll, ll, ll>, vector<tuple<ll, ll, ll>>, greater<tuple<ll, ll, ll>>> q;
+
+    Dijkstra(vector<vector<pair<ll, ll>>> g_, ll n_, ll start=1){
+        g = g_;
+        n = n_;
+        dist.resize(n+1, inf);
+        trace.resize(n+1);
+        dist[start] = 0;
+        vrep(x, g[start]) q.push({x.first, start, x.second});
+    }
+
+    void run(){
+        while (!q.empty()){
+            ll c, pre, v;
+            tie(c, pre, v) = q.top();
+            q.pop();
+            if (dist[v]!=inf) continue;
+            dist[v] = c;
+            trace[v] = pre;
+            vrep(x, g[v]){
+                if (dist[x.second]==inf){
+                    q.push({x.first+dist[v], v, x.second});
+                }
+            }
         }
     }
-    return dist;
-}
+
+    vector<ll> get_dist(){
+        return dist;
+    }
+
+    vector<ll> get_trace(){
+        vector<ll> tr;
+        ll now = n;
+        while (now!=0){
+            tr.pb(now);
+            now = trace[now];
+        }
+        rev(tr);
+        return tr;
+    }
+};
