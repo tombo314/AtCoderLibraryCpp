43f833c53c41233413c1ee857e49d844b712e553
tombo314 <u_tombo@icloud.com>
2023-12-03 00:29:05 +0900

    Update

--- EulerTour.cpp
+++ EulerTour.cpp
@@ -7,14 +7,14 @@ vector<ll> euler_tour(vector<vector<ll>> g, ll root=1){
     while (!q1.empty()){
         ll u = q1[q1.size()-1];
         q1.pop_back();
-        euler.pb(u);
+        euler.push_back(u);
         if (visited[u]) continue;
         vrep(x, g[u]) {
             if (visited[x]){
-                q1.pb(x);
+                q1.push_back(x);
             }
             else{
-                q2.pb(x);
+                q2.push_back(x);
             }
         }
         q1.insert(q1.end(), q2.begin(), q2.end());
