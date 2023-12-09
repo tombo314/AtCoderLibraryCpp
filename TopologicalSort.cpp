4014a3a9cb587e70221c25f5a0a1a5fff1f73618
tombo314 <u_tombo@icloud.com>
2023-12-09 21:12:39 +0900

    Update

--- TopologicalSort.cpp
+++ TopologicalSort.cpp
@@ -1,23 +1,20 @@
+// 1-indexed
 ll n;
 vector<ll> l;
 vector<vector<ll>> g;
-vector<bool> seen(n+1);
+// vector<bool> seen(n+1);
 void dfs(ll v){
     if (!seen[v]){
-        seen[v] = 1;
+        seen[v] = true;
         vrep(x, g[v]){
             dfs(x);
         }
         l.pb(v);
     }
 }
-vector<ll> topological_sort(vector<vector<ll>> g, ll m){
-    /*
-    g: グラフ
-    m: 頂点数
-    */
-    n =  m;
-    rep(i, 1, n+1) dfs(i);
+// m: 頂点数
+vector<ll> topological_sort(ll m){
+    rep(i, 1, m+1) dfs(i);
     rev(l);
     return l;
 }
